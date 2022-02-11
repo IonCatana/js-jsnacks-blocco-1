28c5d4dd4ba3f19261490622b66b3af50d8b65f6
IonCatana <catana.ion17@yahoo.it>
2022-02-11 17:39:49 +0100

    JSnack7 Finito

---
+++ Snack7/app.js
@@ -0,0 +1,10 @@
+// Stampa le potenze di 2 fino a 1000.
+
+// Creo le variabile
+const numeroDue = 2;
+
+for (i = 1; i <= 1000; i++) {
+  const N = [i];
+  potenzeDiDue = numeroDue ** N;
+  console.log(potenzeDiDue, 'potenza di 2');
+}
